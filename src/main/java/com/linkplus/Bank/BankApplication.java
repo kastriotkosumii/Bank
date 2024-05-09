5856f879c54906428e6648223637ca7f418e8372
kastriot <kastriotkosumi@hotmail.com>
2024-05-09 13:36:53 +0200

    Dummie data for BankUser

--- src/main/java/com/linkplus/Bank/BankApplication.java
+++ src/main/java/com/linkplus/Bank/BankApplication.java
@@ -1,13 +1,43 @@
 package com.linkplus.Bank;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.linkplus.Bank.model.BankUser;
+import com.linkplus.Bank.repository.BankUserRepository;
 
 @SpringBootApplication
 public class BankApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BankApplication.class, args);
+	}
+
+	@Bean
+    CommandLineRunner runner(
+            BankUserRepository bankUserRepository) {
+        return args -> {
+            addBankUser(bankUserRepository);
+        };
+    }
+
+	private static void addBankUser(BankUserRepository bankUserRepository){
+
+		BankUser bankUser = new BankUser();
+		bankUser.setEmail("John.Doe@gmail.com");
+		bankUser.setFistName("John");
+		bankUser.setLastName("Doe");
+
+		BankUser bankUser2 = new BankUser();
+		bankUser2.setEmail("filan.fisteku@gmail.com");
+		bankUser2.setFistName("Filan");
+		bankUser2.setLastName("Fisteku");
+		
+		bankUserRepository.save(bankUser);
+		bankUserRepository.save(bankUser2);
+
 	}
 
 }

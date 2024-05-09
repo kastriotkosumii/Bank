d97329ef83a6a168c46226fcef16e9d5552287cb
kastriot <kastriotkosumi@hotmail.com>
2024-05-09 13:48:49 +0200

    Account and its relations to BankUser

---
+++ src/main/java/com/linkplus/Bank/model/Account.java
@@ -0,0 +1,35 @@
+package com.linkplus.Bank.model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Table
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Account {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    private Double accountBalance; 
+
+    @ManyToOne
+    @JoinColumn(name = "bankUser_id")
+    @JsonBackReference
+    private BankUser bankUser;
+}
--- src/main/java/com/linkplus/Bank/model/BankUser.java
+++ src/main/java/com/linkplus/Bank/model/BankUser.java
@@ -1,10 +1,15 @@
 package com.linkplus.Bank.model;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -21,15 +26,20 @@ public class BankUser {
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
-    Long id;
+    private Long id;
 
     @Column(nullable = false)
-    String fistName;
+    private String fistName;
 
     @Column(nullable = false)
-    String lastName;
+    private String lastName;
 
     @Column(nullable = false)
-    String email;
+    private String email;
+
+    @OneToMany(mappedBy = "bankUser")
+    @Column(nullable = false)
+    @JsonManagedReference
+    private List<Account> userAccount;
     
 }
